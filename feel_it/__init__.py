3c5ef2563188339b3bd056fff44662cda40c4284
vinid <chiccobia@gmail.com>
2021-03-18 12:16:49 +0100

    Bump version: 1.0.1 → 1.0.2

--- feel_it/__init__.py
+++ feel_it/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Federico Bianchi"""
 __email__ = 'f.bianchi@unibocconi.it'
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 from .feel_it import SentimentClassifier
 from .feel_it import EmotionClassifier
