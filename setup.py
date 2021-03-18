3bced00310fc7b15a24e905bd92a6433eaa43d50
vinid <chiccobia@gmail.com>
2021-03-18 12:22:46 +0100

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MilaNLProc/feel-it',
-    version='1.0.2',
+    version='1.0.3',
     zip_safe=False,
 )
